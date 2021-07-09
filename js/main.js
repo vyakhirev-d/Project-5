991751bf53aedf70ae0946999f861fb19a8bd1f6
vyakhirev-d <69190976+vyakhirev-d@users.noreply.github.com>
2021-07-09 09:34:43 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,19 @@ $('[data-fancybox]').fancybox({
     showinfo: 0
   }
   });
+  
+  $('.heroes__slider-img').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.heroes__slider-text',
+    prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
+  });
+  $('.heroes__slider-text').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    arrows: false,
+    asNavFor: '.heroes__slider-img',
+    });
 });
